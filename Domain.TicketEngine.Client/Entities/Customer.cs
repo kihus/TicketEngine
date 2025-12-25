cd3fde2f6d56d2821d06dff137bccb87d4f6e753
kihus <wintergamesk@gmail.com>
2025-12-25 16:01:55 -0300

    feat: implement `Customer`

--- Domain.TicketEngine.Client/Entities/Customer.cs
+++ Domain.TicketEngine.Client/Entities/Customer.cs
@@ -1,5 +1,44 @@
-﻿namespace Domain.TicketEngine.CustomerApi.Entities;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
-public class Customer
+namespace Domain.TicketEngine.CustomerApi.Entities;
+
+public class Customer(
+    string name,
+    string lastName,
+    DateOnly birthdate,
+    string document,
+    string email,
+    string password
+    )
 {
+    [BsonId]
+    public ObjectId Id { get; init; } = ObjectId.GenerateNewId();
+    public string Name { get; private set; } = name;
+    public string LastName { get; private set; } = lastName;
+    public DateOnly Birthdate { get; private set; } = birthdate;
+    public string? Phone { get; private set; } = null;
+    public string Document { get; private set; } = document;
+    public string Email { get; private set; } = email;
+    public string Password { get; private set; } = password;
+
+    public Customer(
+        string name,
+        string lastName,
+        DateOnly birthdate,
+        string? phone, 
+        string document,
+        string email,
+        string password
+        ) : this (
+            name, 
+            lastName, 
+            birthdate, 
+            document, 
+            email, 
+            password
+            )
+    {
+        Phone = phone;
+    }
 }
--- Domain.TicketEngine.Client/Extensions/CustomerExtension.cs
+++ Domain.TicketEngine.Client/Extensions/CustomerExtension.cs
@@ -1,6 +1,15 @@
-﻿namespace Domain.TicketEngine.CustomerApi.Extensions;
+﻿using Domain.TicketEngine.CustomerApi.DTOs;
+using Domain.TicketEngine.CustomerApi.Entities;
 
-public class CustomerExtension
+namespace Domain.TicketEngine.CustomerApi.Extensions;
+
+public static class CustomerExtension
 {
+    public static CustomerResponseDto ToDto(this Customer customer)
+    {
+        if (customer is null)
+            return null;
 
+        return new CustomerResponseDto();
+    }
 }
