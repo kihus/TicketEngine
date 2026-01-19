eddcd5fedae00b9dff2301eaf03c64d4ec4ce72f
kihus <wintergamesk@gmail.com>
2026-01-19 10:48:03 -0300

    refactor: Bootstrap

---
+++ Infrastructure.Data/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Data.Bearer_Token.Settings;
+using Infrastructure.Data.Mongo.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Data.Extensions;
+
+public static class Bootstrap
+{
+	public static IServiceCollection AddMongoSettings(this IServiceCollection service, IConfiguration configuration)
+	{
+		var settings = configuration.GetSection("MongoSettings").Get<MongoDbSettings>();
+
+		return service.AddSingleton(settings ?? throw new ArgumentNullException("Connection null error"));
+	}
+
+	public static IServiceCollection AddTokenSettings(this IServiceCollection service, IConfiguration configuration)
+	{
+		var settings = configuration.GetSection("JwtSettings").Get<TokenSettings>();
+		return service.AddSingleton(settings ?? throw new ArgumentNullException("Token configuration error"));
+	}
+}
