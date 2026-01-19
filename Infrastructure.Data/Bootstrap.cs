ad2085ffc3b5f72248f0a08d58f7f2eb6ccbd51f
kihus <wintergamesk@gmail.com>
2026-01-19 12:58:10 -0300

    refactor: remove Token authenticaction from Program and move to Bootstrap

--- Infrastructure.Data/Bootstrap.cs
+++ Infrastructure.Data/Bootstrap.cs
@@ -1,7 +1,12 @@
 ﻿using Infrastructure.Data.Bearer_Token.Settings;
+using Infrastructure.Data.Mongo;
 using Infrastructure.Data.Mongo.Settings;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Infrastructure.Data;
 
@@ -11,7 +16,9 @@ public static class Bootstrap
 	{
 		var settings = configuration.GetSection("MongoSettings").Get<MongoDbSettings>();
 
-		return service.AddSingleton(settings ?? throw new ArgumentNullException("Connection null error"));
+		return service
+			.AddSingleton(settings ?? throw new ArgumentNullException("Connection null error"))
+			.AddSingleton<MongoContext>();
 	}
 
 	public static IServiceCollection AddTokenSettings(this IServiceCollection service, IConfiguration configuration)
@@ -19,4 +26,31 @@ public static class Bootstrap
 		var settings = configuration.GetSection("JwtSettings").Get<TokenSettings>();
 		return service.AddSingleton(settings ?? throw new ArgumentNullException("Token configuration error"));
 	}
+
+	public static AuthenticationBuilder AddTokenAuthentication(this IServiceCollection service, IConfiguration configuration)
+	{
+		var authentication = new AuthenticationOptions();
+		var tokenKey = Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"] ?? string.Empty);
+		var jwtBearerOptions = new JwtBearerOptions();
+
+		authentication.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		authentication.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+		jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+		{
+			ValidateLifetime = true,
+			ClockSkew = TimeSpan.Zero,
+			ValidateAudience = true,
+			ValidAudience = configuration["JwtSettings:Audience"],
+			ValidateIssuer = true,
+			ValidIssuer = configuration["JwtSettings:Issuer"],
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(tokenKey)
+		};
+
+		return service
+				.AddAuthentication(options => { options = authentication; })
+				.AddJwtBearer(options => { options = jwtBearerOptions; });
+	}
+
 }
--- TicketEngine.UserApi/Program.cs
+++ TicketEngine.UserApi/Program.cs
@@ -1,10 +1,6 @@
 using Infrastructure.Data;
 using Infrastructure.Data.Bearer_Token;
 using Infrastructure.Data.Bearer_Token.Interfaces;
-using Infrastructure.Data.Mongo;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using UserApi.Repositories.v1;
 using UserApi.Repositories.v1.Interfaces;
 using UserApi.Services.v1;
@@ -16,32 +12,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddMongoSettings(builder.Configuration);
 builder.Services.AddTokenSettings(builder.Configuration);
-
-builder.Services.AddAuthentication(options =>
-{
-	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-	var configuration = builder.Configuration;
-	var tokenKey = Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"] ?? string.Empty);
-
-	options.TokenValidationParameters = new TokenValidationParameters()
-	{
-		ValidateLifetime = true,
-		ClockSkew = TimeSpan.Zero,
-		ValidateAudience = true,
-		ValidAudience = configuration["JwtSettings:Audience"],
-		ValidateIssuer = true,
-		ValidIssuer = configuration["JwtSettings:Issuer"],
-		ValidateIssuerSigningKey = true,
-		IssuerSigningKey = new SymmetricSecurityKey(tokenKey)
-	};
-});
+builder.Services.AddTokenAuthentication(builder.Configuration);
 
 builder.Services.AddAuthorization();
-
-builder.Services.AddSingleton<MongoContext>();
 
 builder.Services.AddScoped<ICustomerRepository, CustomerRespository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
